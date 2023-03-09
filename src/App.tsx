41f359d7c8356c5e3223b24711dc6a2080c005b1
rida chebli <rchebli@oneroyal.com>
2023-03-09 16:08:40 +0200

    B

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+// import '/js/jquery.min.js';
+// import '/js/bootstrap.bundle.min.js';
+// import '/js/custom.min.js';
+
+
 import React from "react";
 import Footersection from "./components/Footer";
 import Menu from "./components/Menu";
 import Topnav from "./components/Topnav";
 import Content from "./components/Content";
+
+
 
 export default class App extends React.Component {
 
